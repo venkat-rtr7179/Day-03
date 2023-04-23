c0bdc2cfebe14a4552e7c04c7f3f1c71b6595c7f
venkat-rtr7179 <125110848+venkat-rtr7179@users.noreply.github.com>
2023-04-23 12:32:46 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,9 @@
+var Resume={
+    "name":"john",
+    "age":"23"
+    "Martial Status":"unmarried"
+    "Profile":"Messaging administrator"
+    "contact":"7305324523"
+    "address":"chennai"
+  };
+  
